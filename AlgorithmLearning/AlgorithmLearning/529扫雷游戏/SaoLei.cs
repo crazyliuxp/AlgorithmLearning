4b0864b965ea55015a204e86b1a51e67f3261d3b
LiuXiaoPeng <liuxiaopeng@mediinfo.com.cn>
2020-08-20 16:26:57 +0800

    修改int转char方法

--- AlgorithmLearning/AlgorithmLearning/529扫雷游戏/SaoLei.cs
+++ AlgorithmLearning/AlgorithmLearning/529扫雷游戏/SaoLei.cs
@@ -1,9 +1,10 @@
 public class SaoLei {
 
-    // 1.当前点击的是【未挖出来的雷】，我们将其改为X即可
+    // 1.当前点击的是【未挖出来的雷】，我们将其改为X即可,表示点到了雷，然后结束。
     // 2.当前点击的是【为挖出的空方块】，我们需要统计他周围相邻（八个）的方块里的地雷的数量cnt（即M的数量）。
+    //   如果cnt>0，就将当前方块改为cnt的数字(附近的雷数)
     //   如果cnt为0，则将其改为B，并递归计算周围八个未挖出的方块，如果没有更多方块可以被揭露，则返回面板。
-    //   如果cnt不等于0，就将其改为cnt的数字
+
     public char[][] UpdateBoard (char[][] board, int[] click) {
         int x = click[0], y = click[1];
 
@@ -21,7 +22,7 @@ public class SaoLei {
     int[] vx = new int[] {-1, -1, 0, 1, 1, 1, 0, -1 };
     int[] vy = new int[] { 0, -1, -1, -1, 0, 1, 1, 1 };
     private void dfs (char[][] board, int x, int y) {
-        //计算当前点[x,y]周围M的数量
+        //计算当前点[x,y]周围M(雷)的数量
         int cnt = 0;
         for (int i = 0; i < 8; i++) {
             int currentX = x + vx[i];
@@ -40,8 +41,10 @@ public class SaoLei {
 
         //周围M个数不为0时，将[x,y]处的值设置成cnt数字
         if (cnt > 0) {
-            board[x][y] = (char) cnt;
-        } else {
+            board[x][y] = (char) (cnt + '0');
+        } 
+        //如果周围没有M(雷)时
+        else {
             //将[x,y]初的值设置成B
             board[x][y] = 'B';
 
