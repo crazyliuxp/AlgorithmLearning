e56c86e08de9d0d75e3dad16731ab514a8ecb3d3
LiuXiaoPeng <liuxiaopeng@mediinfo.com.cn>
2020-08-20 15:57:24 +0800

    529扫雷游戏

--- AlgorithmLearning/AlgorithmLearning/Program.cs
+++ AlgorithmLearning/AlgorithmLearning/Program.cs
@@ -1,17 +1,15 @@
 ﻿using AlgorithmLearning._647回文子串;
 using System;
 
-namespace AlgorithmLearning
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //回文子串
-            Leetcode_CountSubstrings count = new Leetcode_CountSubstrings();
-            string s = "aaa";
-            int num= count.CountSubstrings(s);
-            Console.WriteLine(num);
+namespace AlgorithmLearning {
+    class Program {
+        static void Main (string[] args) {
+            
+            // //回文子串
+            // Leetcode_CountSubstrings count = new Leetcode_CountSubstrings();
+            // string s = "aaa";
+            // int num= count.CountSubstrings(s);
+            // Console.WriteLine(num);
 
         }
     }
