a4b23c7b675ce81788e7e9230b3a1f5f71b97927
LiuXiaoPeng <liuxiaopeng@mediinfo.com.cn>
2020-08-31 09:47:24 +0800

    841.钥匙和房间

---
+++ AlgorithmLearning/AlgorithmLearning/841钥匙和房间/KeyAndRoom.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmLearning._841钥匙和房间
+{
+    /// <summary>
+    /// 841. 钥匙和房间
+    /// https://leetcode-cn.com/problems/keys-and-rooms/
+    /// </summary>
+    public class KeyAndRoom
+    {
+        private bool[] vis;
+        private int num;
+        public bool CanVisitAllRooms(IList<IList<int>> rooms)
+        {
+            int len = rooms.Count;
+            vis = new bool[len];
+            num = 0;
+
+            //从第0个房间开始
+            dfs(rooms, 0);
+
+            return num == len;
+        }
+
+        private void dfs(IList<IList<int>> rooms, int index)
+        {
+            vis[index] = true;
+            num++;
+            
+            foreach(var roomNum in rooms[index]){
+                if (!vis[roomNum])
+                {
+                    dfs(rooms, roomNum);
+                }
+            }
+        }
+    }
+}
