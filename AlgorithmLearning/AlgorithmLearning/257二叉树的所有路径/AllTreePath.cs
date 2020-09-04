60f6e054f695098163af103efeeb12639f755d94
LiuXiaoPeng <liuxiaopeng@mediinfo.com.cn>
2020-09-04 09:35:48 +0800

    二叉树的所有路径

---
+++ AlgorithmLearning/AlgorithmLearning/257二叉树的所有路径/AllTreePath.cs
@@ -0,0 +1,37 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmLearning._257二叉树的所有路径
+{
+    public class AllTreePath
+    {
+        public class Solution
+        {
+            public IList<string> BinaryTreePaths(TreeNode root)
+            {
+                var result = new List<string>();
+                if (root == null) return result;
+                var currentPath = string.Empty;
+
+                dfs(root, currentPath, result);
+
+                return result;
+            }
+
+            private void dfs(TreeNode root, string currentPath, IList<string> result)
+            {
+                if (root == null) return;
+
+                if(root.left==null && root.right == null)
+                {
+                    result.Add(currentPath + root.val);
+                }
+
+                dfs(root.left, currentPath + root.val + "->", result);
+                dfs(root.right, currentPath + root.val + "->", result);
+            }
+        }
+    }
+}
